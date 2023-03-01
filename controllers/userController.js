fc5d25cd947519f96c1a2943cfbede80b7f806b5
eliasraffaelli0 <eliasraffaelli@gmail.com>
2023-03-01 17:14:31 -0300

    password hashing working and emails duplicate validation added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,14 @@
 const User = require('../models/user');
 const {body, validationResult} = require('express-validator');
+const bcrypt = require('bcrypt');
+
 
 exports.sign_up_get = (req, res, next) => {
     res.render("sign_up_form");
 };
 
 exports.sign_up_post = [
+    // inputs sanitization
     body('first_name')
         .trim()
         .isLength({ min:2})
@@ -23,7 +26,23 @@ exports.sign_up_post = [
     body('email')
         .trim()
         .isEmail()
-        .withMessage('Insert a valid email'),
+        .withMessage('Insert a valid email')
+        //check if the email is already in the database
+        .custom((value)=>{
+            return User.findOne({email:value}).then(user => {
+                if (user) {
+                    return Promise.reject('Email already registered');
+                }
+            });
+            // User.findOne({email:value}, (err,user)=>{
+            //     if(user){
+            //         console.log('kaka')
+            //         return false;
+            //         // throw new Error('Password confirmation does not match password');
+            //     }
+            // })
+            // return true;
+        }),
     body('password')
         .isLength({min:5})
         .withMessage('Password must be a least 5 characters long'),
@@ -35,7 +54,7 @@ exports.sign_up_post = [
     }),
     (req, res, next)=>{
         const errors = validationResult(req);
-
+        
         if(!errors.isEmpty()){
             res.render("sign_up_form", {
                 title: "Create author",
@@ -44,18 +63,22 @@ exports.sign_up_post = [
             });
             return;
         }
-
-        const user = new User({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            email: req.body.email,
-            password: req.body.password
-        });
-        user.save((err)=>{
-            if(err){
-                return next(err);
-            }
-            res.redirect("/")
+        //if there is no errors then i hash the password and then stored it
+        bcrypt.hash(req.body.password,10, (err, hash)=>{
+            const user = new User({
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email.toLowerCase(),
+                password: hash
+            });
+            user.save((err)=>{
+                if(err){
+                    return next(err);
+                }
+                res.redirect("/")
+            })
         })
+
+
     }
 ];
