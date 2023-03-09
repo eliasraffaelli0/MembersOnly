61a36b5e96235908d3a3a2ef51bb38c87b1f60f9
eliasraffaelli0 <eliasraffaelli@gmail.com>
2023-03-09 14:46:54 -0300

    changed express-session to cookie-session to use in production

--- controllers/authController.js
+++ controllers/authController.js
@@ -104,10 +104,6 @@ exports.login_post = passport.authenticate('local',  {
 });
 
 exports.logout_get = (req, res, next) => {
-    req.logout((err)=>{
-        if(err){
-            return next(err);
-        }
-        res.redirect('/');
-    });
+    req.logout();
+    res.redirect("/");
 };
